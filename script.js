e64f1f73c0fc157d355d8913ecc492e525a269a4
SkyeYang001 <143848467+SkyeYang001@users.noreply.github.com>
2024-02-12 19:54:49 -0800

    Homework
    2/12/24

--- script.js
+++ script.js
@@ -1,57 +1,79 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    document.getElementById("Option-Form").addEventListener("submit", function (eventData) {
+    document.getElementById("Option-Form").addEventListener('submit', function (eventData) {
         eventData.preventDefault();
         console.log(eventData.target);
         var formData = new FormData(eventData.target);
         formData = Object.fromEntries(formData);
-        changeCert = submitCert(eventData.target.value);
 
         let presentOutput = document.getElementById('Certification-Present');
         let nameOutput = document.getElementById('Certification-Name');
         let detailOutput = document.getElementById('Certification-Details');
         let iconOutput = document.getElementById('Certification-Icon');
         let ownerOutput = document.getElementById('Certification-Owner');
- 
+
 
         presentOutput.innerText = `${formData.option}`;
         nameOutput.innerText = formData.name;
         detailOutput.innerText = `${formData.option}`;
         iconOutput = formData.option;
         ownerOutput.innerText = `${formData.option}`;
-        /*let dateOutput = document.getElementById("Certification-Date");
+        
+        let dateOutput = document.getElementById("Certification-Date");
         let theDate = new Data().toLocaleDataString('en-us', {weekday: "long", year:"numeric", month:"short", day:"numeric"})
-        dateOutput.innerText = `${theDate}`; */
+        dateOutput.innerText = `${theDate}`;
 
-      });
-      
-    function submitCert(value){
+    });
+
+    document.getElementById("option").addEventListener('change', function (eventData) {
+        changeCert = submitCert(eventData.target.value);
+
+    });
+
+    document.getElementById("name").addEventListener('submit', function (eventData) {
+        let nameOutput = document.getElementById('Certification-Name');
+        nameOutput.innerText = eventData.target.value;
+
+    });
+
+    function submitCert(value) {
         let option = document.getElementById('Certification-Paper');
         option.classList = "";
-        switch(value){
+        switch (value) {
             case 'A':
                 option.classList.add('a');
                 console.log('a');
-            break;
+                break;
             case 'B':
                 option.classList.add('b');
                 console.log('b');
-            break;
+                break;
             case 'C':
                 option.classList.add('c');
                 console.log('c');
-            break;
+                break;
+            case 'X':
+                option.classList.add('x');
+                break;
         }
     }
 
-if ( document.getElementById('Certification-Paper') !== !null) {
-    let printelement = document.getElementById('Certification-Paper');
-    printelement.addEventListener('click', function(e) {
-        window.print();
-        console.log('invoke print');
-    });
-    //in html -> onclick="windowprint()"
-}
+    if (document.getElementById('Certification-Paper') !== !null) {
+
+        let printelement = document.getElementById('Certification-Paper');
+
+        printelement.addEventListener('click', function (e) {
+            var printcontent = document.getElementById('Certification-Paper');
+
+            var printpaper = window.open('', '', 'left=0,top=0,width=1500,height=1000,toolbar=0,scrollbars=0,status=0');
+            printpaper.document.write(printcontent.innerHTML);
+            printpaper.document.close();
+            printpaper.focus();
+            printpaper.print();
+            printpaper.close();
+        });
+        //in html -> onclick="windowprint()"
+    }
 
     /*if (document.getElementById("option") = document.getElementById("A")) {
         const presentOutputA = document.getElementById("Certification-Present");
